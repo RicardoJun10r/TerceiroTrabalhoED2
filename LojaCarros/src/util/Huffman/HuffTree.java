3d4b1befb43c624ffef83326dfe9981b0b6c0977
RicardoJun10r <ricardojunior0110@gmail.com>
2023-10-13 09:44:34 -0300

    att

--- LojaCarros/src/util/Huffman/HuffTree.java
+++ LojaCarros/src/util/Huffman/HuffTree.java
@@ -10,8 +10,6 @@ public class HuffTree implements Serializable {
 
     private Map<Character, Integer> map;
 
-    private HuffNode[] vetor;
-
     private HuffNode raiz;
 
     private Character[] texto;
@@ -28,18 +26,10 @@ public class HuffTree implements Serializable {
     }
 
     public void Clear(){
-        ClearVector();
         ClearTree(this.raiz);
         this.map.clear();
         this.texto = null;
     }
-
-    private void ClearVector(){
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = null;
-        }
-    }
-
 
     private void ClearTree(HuffNode no){
         if(no != null){
@@ -58,20 +48,6 @@ public class HuffTree implements Serializable {
             } else {
                 this.map.put(texto[i], freq);
             }
-        }
-
-        this.vetor = new HuffNode[this.map.size()];
-
-    }
-
-    private void BuildVector(){
-
-        Set<Entry<Character, Integer>> set = this.map.entrySet();
-
-        int cont = 0;
-        for (Entry<Character,Integer> entry : set) {
-            this.vetor[cont] = new HuffNode(entry.getKey(), entry.getValue());
-            cont++;
         }
 
     }
@@ -153,36 +129,6 @@ public class HuffTree implements Serializable {
             Search(no.getDir(), caractere, stringBuffer);
         }
 
-        // if(no.getDir().getCaractere().indexOf(caractere) == -1){
-        //     stringBuffer.append("0");
-        //     Search(no.getEsq(), caractere, stringBuffer);
-        // } else {
-        //     stringBuffer.append("1");
-        //     Search(no.getDir(), caractere, stringBuffer);
-        // }
-    }
-
-    private void shellSort(){
-
-        int H = 1;
-
-        while(H < this.vetor.length){ H = 3*H+1; }
-
-        while(H > 0){
-
-            for(int i = H; i < this.vetor.length; i++){
-                HuffNode aux = this.vetor[i];
-                int j = i;
-                while(j > H - 1 && this.vetor[j - H].getFrequencia() > aux.getFrequencia()){
-                    this.vetor[j] = this.vetor[j - H];
-                    j = j - H;
-                }
-                this.vetor[j] = aux;
-            }
-
-            H = (int) Math.floor(H / 3);
-        }
-
     }
 
     private void BuildTree(){
@@ -190,11 +136,8 @@ public class HuffTree implements Serializable {
         Freq();
 
         BuildHeap();
-
-        // shellSort();
         
         for (int i = 0; i < contador; i++) {
-            // Add(this.vetor[i]);
             Add(this.heapMan.Delete());
         }
 
@@ -243,14 +186,6 @@ public class HuffTree implements Serializable {
     private Boolean Empty(){
         if(this.raiz == null) return true;
         else return false;
-    }
-
-    public void PrintVector(){
-        System.out.println("=================");
-        for (int i = 0; i < this.vetor.length; i++) {
-            System.out.println(this.vetor[i].getCaractere() + " = " + this.vetor[i].getFrequencia());
-        }
-        System.out.println("=================");
     }
 
     public void Print(){
